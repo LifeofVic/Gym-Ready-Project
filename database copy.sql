58235cfbefc033e910ec1806acf9ab065a217740
Victor Llapa <victorllapa@gmail.com>
2023-01-01 18:46:09 -0600

    Added sql for the database for the app.

--- database copy.sql
+++ database copy.sql
@@ -1,13 +1,3 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
--- CREATE TABLE "user" (
---     "id" SERIAL PRIMARY KEY,
---     "username" VARCHAR (80) UNIQUE NOT NULL,
---     "password" VARCHAR (1000) NOT NULL
--- );
 CREATE TABLE set (
 	"id" 						integer PRIMARY KEY UNIQUE,
 	"session_id" 		integer NOT NULL,
