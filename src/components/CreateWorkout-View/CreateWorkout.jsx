f9d874308a74bbbb5a24145483149500e6723327
Victor Llapa <victorllapa@gmail.com>
2023-01-03 19:54:25 -0600

    Added some styling and toggle buttons for the days of the week in the create workout view

--- src/components/CreateWorkout-View/CreateWorkout.jsx
+++ src/components/CreateWorkout-View/CreateWorkout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../CreateWorkout-View/CreateWorkout.css';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { number } from 'prop-types';
 
 export default function CreateWorkout() {
 	const GroupedExercise = useSelector(store => store.exercise);
@@ -16,6 +17,7 @@ export default function CreateWorkout() {
 		<>
 			<div className='filter-section' onChange={CategoryExercise}>
 				<select placeholder='Muscle Group'>
+					<option>Muscle Group... </option>
 					<option value='chest'>Chest </option>
 					<option value='back'>Back </option>
 					<option value='cardio'>Cardio </option>
@@ -28,14 +30,53 @@ export default function CreateWorkout() {
 					<option value='upper legs'>Upper Legs </option>
 				</select>
 
-				<input placeholder='Exercise'></input>
-				<input placeholder='# of sets'></input>
+				<select placeholder='Exercise' className='exercise-list'>
+					<option>exercise selection... </option>
+				</select>
+
+				<input type='number' placeholder='# of sets'></input>
 			</div>
 			<div className='exercise-detail'>
 				{' '}
 				{JSON.stringify(GroupedExercise.data)}
 			</div>
-			<div className='day-section'></div>
+			<div className='day-section'>
+				<label class='switch'>
+					<input type='checkbox' value='Sunday' />
+					<span class='slider round'>Sunday</span>
+				</label>
+
+				<label class='switch'>
+					<input type='checkbox' value='Monday' />
+					<span class='slider round'>Monday</span>
+				</label>
+
+				<label class='switch'>
+					<input type='checkbox' value='Tuesday' />
+					<span class='slider round'>Tuesday</span>
+				</label>
+
+				<label class='switch'>
+					<input type='checkbox' value='Wednesday' />
+					<span class='slider round'>Wednesday</span>
+				</label>
+
+				<label class='switch'>
+					<input type='checkbox' value='Thursday' />
+					<span class='slider round'>Thursday</span>
+				</label>
+
+				<label class='switch'>
+					<input type='checkbox' value='Friday' />
+					<span class='slider round'>Friday</span>
+				</label>
+
+				<label class='switch'>
+					<input type='checkbox' value='Saturday' />
+					<span class='slider round'>Saturday</span>
+				</label>
+			</div>
+			<button className='submit-btn'> Add Exercise</button>
 		</>
 	);
 }
---
+++ src/components/CreateWorkout-View/ExerciseList.jsx
@@ -0,0 +1,1 @@
+//! This will generate the <option> </option> for each of the exercise being filterd by the muscle group to appear onto the drop down menu.
