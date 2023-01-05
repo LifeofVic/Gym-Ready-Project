d71e9bcc3f0d3bbb022023d04c539003a1a1b8e2
Victor Llapa <victorllapa@gmail.com>
2023-01-05 13:04:16 -0600

    Removed code that was not no longer being used

--- server/routes/exercise.router.js
+++ server/routes/exercise.router.js
@@ -3,26 +3,6 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-//! This is no being longer being used, initial testing purpose
-// router.get('/', (req, res) => {
-// 	console.log('In the exercise.Router', req.body);
-// 	//let randomNumber = Math.random() * (1327 - 1) + 1; //!Not working
-// 	const queryText = `SELECT "exercise_name" FROM "exercise" WHERE "id" = 34;`;
-// 	console.log('In Exercise router!');
-// 	pool
-// 		.query(queryText)
-// 		.then(result => {
-// 			res.send(result.rows);
-// 		})
-// 		.catch(err => {
-// 			console.log(
-// 				'Error in getting a random Exercise from DB using exercise.router: ',
-// 				err
-// 			);
-// 			res.sendStatus(500);
-// 		});
-// });
-
 //This will run the SQL that will also contain the string for the muscle_group.
 //The result for this will contain an array of objects that each object will hold the exercises
 // correlated wit the same muscle_group name.
@@ -44,27 +24,6 @@ router.get('/:musclegroup', (req, res) => {
 			res.sendStatus(500);
 		});
 }); //End of Router.Get
-
-// router.get('/:exerciseName', (req, res) => {
-// 	const exerciseName = req.params.exerciseName;
-// 	console.log(
-// 		'Going to look for the object containing the property [exercise_name ] = ',
-// 		exerciseName
-// 	);
-// 	const queryText = `SELECT * FROM "exercise" WHERE "exercise_name" = $1;`;
-
-// 	pool
-// 		.query(queryText, [exerciseName])
-// 		.then(result => {
-// 			res.send(result.rows);
-// 			console.log('Result rows: [exercise object]', result.rows);
-
-// 		})
-// 		.catch(error => {
-// 			console.log('Error in GET /:exerciseGif / exercise.Router', error);
-// 			res.sendStatus(500);
-// 		});
-// });
 
 /**
  * POST route template
--- src/redux/reducers/exercise.reducer.js
+++ src/redux/reducers/exercise.reducer.js
@@ -12,16 +12,4 @@ const exerciseGroup = (state = [], action) => {
 	}
 };
 
-// const displayExercise = (state = [], action) => {
-// 	switch (action.type) {
-// 		case 'DISPLAY_EXERCISE':
-// 			console.log('Exercise reducer: ', action.payload);
-// 			return action.payload;
-// 		case 'CLEAR_DISPLAY_EXERCISE':
-// 			return [];
-// 		default:
-// 			return state;
-// 	}
-//};
-
 export default exerciseGroup;
