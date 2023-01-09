f5a43abb8822131447b25ad12793c691d9ccbc6a
Victor Llapa <victorllapa@gmail.com>
2023-01-09 15:46:34 -0600

    starter layout for the insert exercise form

--- src/components/InsertNewExercise/InsertNewExercise.jsx
+++ src/components/InsertNewExercise/InsertNewExercise.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import '../CreateWorkout-View/CreateWorkout.css';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
+import '../InsertNewExercise/InsertExercise.css';
+import { Card } from '@mui/material';
 
 export default function InsertNewExercise() {
 	console.log('In InsertNewExercise.JSX');
@@ -35,44 +36,55 @@ export default function InsertNewExercise() {
 		<>
 			<h2>THIS IS THE INSERT FORM</h2>
 			<h5>This will send the inputs to the database</h5>
-			<select className='new-exercises-options' onChange={setExerciseByGroup}>
-				<option>Select Muscle Group</option>
-				<option value='chest'>Chest </option>
-				<option value='back'>Back </option>
-				<option value='cardio'>Cardio </option>
-				<option value='lower arms'>Lower Arms </option>
-				<option value='waist'>Waist </option>
-				<option value='shoulders'>Shoulders </option>
-				<option value='lower legs'>Lower Legs </option>
-				<option value='neck'>Neck </option>
-				<option value='upper arms'>Upper Arms </option>
-				<option value='upper legs'>Upper Legs </option>
-			</select>
 
-			<select
-				className='new-exercises-options'
-				onChange={setExerciseByTargetValue}>
-				<option placeholder='Target Muscle'>Select</option>
-				{GroupOptions.map((exercise, index) => {
-					return (
-						<option
-							className='target-options'
-							key={index}
-							value={exercise.muscle_target}>
-							{exercise.muscle_target}
-						</option>
-					);
-				})}
-			</select>
+			<div className='content'>
+				<select className='new-exercises-options' onChange={setExerciseByGroup}>
+					<option>Select Muscle Group</option>
+					<option value='chest'>Chest </option>
+					<option value='back'>Back </option>
+					<option value='cardio'>Cardio </option>
+					<option value='lower arms'>Lower Arms </option>
+					<option value='waist'>Waist </option>
+					<option value='shoulders'>Shoulders </option>
+					<option value='lower legs'>Lower Legs </option>
+					<option value='neck'>Neck </option>
+					<option value='upper arms'>Upper Arms </option>
+					<option value='upper legs'>Upper Legs </option>
+				</select>
 
-			<input
-				id='url_input'
-				placeholder='GIF URL'
-				type='text'
-				onChange={setExerciseURL}
-			/>
+				<select
+					className='new-exercises-options'
+					onChange={setExerciseByTargetValue}>
+					<option placeholder='Target Muscle'>Select</option>
+					{GroupOptions.map((exercise, index) => {
+						return (
+							<option
+								className='target-options'
+								key={index}
+								value={exercise.muscle_target}>
+								{exercise.muscle_target}
+							</option>
+						);
+					})}
+				</select>
 
-			<Button variant='contained'>Submit</Button>
+				<input
+					id='user-input'
+					placeholder='Exercise Name'
+					type='text'
+					onChange={setExerciseURL}
+				/>
+
+				<input
+					id='user-input'
+					placeholder='GIF URL'
+					type='text'
+					onChange={setExerciseURL}
+				/>
+			</div>
+			<div>
+				<Button variant='contained'>Submit</Button>
+			</div>
 		</>
 	);
 }
