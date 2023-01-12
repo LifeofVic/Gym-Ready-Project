bbe698ee18c5db7f5f659f4cbc4eba32d946013c
Victor Llapa <victorllapa@gmail.com>
2023-01-11 23:07:35 -0600

    working on nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,46 +4,74 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
 
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+
 function Nav() {
 	const user = useSelector(store => store.user);
+	const [value, setValue] = React.useState(0);
 
 	return (
-		<div className='nav'>
+		<Box className='nav'>
 			{/* <Link to='/home'>
 				<h2 className='nav-title'>Gym Ready</h2>
 			</Link> */}
-			<div>
+			<BottomNavigation
+				showLabels
+				value={value}
+				onChange={newValue => {
+					setValue(newValue);
+				}}>
 				{/* If no user is logged in, show these links */}
 				{!user.id && (
 					// If there's no user, show login/registration links
-					<Link className='navLink' to='/login'>
+					<BottomNavigationAction
+						className='navLink'
+						value='/login'
+						label='Login / Register'>
 						Login / Register
-					</Link>
+					</BottomNavigationAction>
 				)}
 
 				{/* If a user is logged in, show these links */}
 				{user.id && (
 					<>
-						<Link className='navLink' to='/user'>
-							Home
-						</Link>
+						<BottomNavigationAction
+							className='navLink'
+							value='/user'
+							label='user'
+							component={Link}
+							to='/user'></BottomNavigationAction>
 
-						<Link className='navLink' to='/favorite'>
-							Favorite
-						</Link>
+						<BottomNavigationAction
+							className='navLink'
+							value='/favorite'
+							label='Favorite'
+							component={Link}
+							to='/favorite'></BottomNavigationAction>
 
-						<Link className='navLink' to='/search-exercise'>
-							Search Exercise
-						</Link>
-						<LogOutButton className='navLink' />
+						<BottomNavigationAction
+							className='navLink'
+							value='/search-exercise'
+							label='Search Exercise'
+							component={Link}
+							to='/search-exercise'></BottomNavigationAction>
+
+						<BottomNavigationAction>
+							<LogOutButton className='navLink' />
+						</BottomNavigationAction>
 					</>
 				)}
 
-				<Link className='navLink' to='/about'>
+				<BottomNavigationAction
+					className='navLink'
+					value='/about'
+					label='About'>
 					About
-				</Link>
-			</div>
-		</div>
+				</BottomNavigationAction>
+			</BottomNavigation>
+		</Box>
 	);
 }
 
--- src/components/UserFavorites/ViewFavorite.jsx
+++ src/components/UserFavorites/ViewFavorite.jsx
@@ -4,9 +4,11 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Stack from '@mui/material/Stack';
 
 import '../UserFavorites/ViewFavorite.css';
 import { useHistory } from 'react-router-dom';
+import { Grid, MenuItem } from '@mui/material';
 
 //TODO This will display the View page for the users workout for the day.
 //? Will have to insert workouts for each user in order to display them onto the view page.
@@ -85,13 +87,15 @@ export default function ViewFavorite() {
 				{data.map((exercise, index) => {
 					if (exercise.like == true) {
 						return (
-							<table className='Exercise-content-liked'>
-								<tr key={exercise.id} id='row-content'>
-									<td id='name'>{exercise.exercise_name} </td>
-									<td id='target-muscle'> {exercise.muscle_target} </td>
-								</tr>
-								<tr className='button-options'>
-									<td>
+							<Grid className='Exercise-content-liked'>
+								<Grid key={exercise.id} id='row-content'>
+									<MenuItem id='name'>{exercise.exercise_name} </MenuItem>
+									<MenuItem id='target-muscle'>
+										{exercise.muscle_target}
+									</MenuItem>
+								</Grid>
+								<Grid alignItems='center'>
+									<Stack className='button-options' direction='row' gap={4}>
 										<Button
 											className='edit-btn'
 											id='popUp'
@@ -100,8 +104,6 @@ export default function ViewFavorite() {
 											onClick={UpdateLike}>
 											❤️
 										</Button>
-									</td>
-									<td>
 										<Button
 											className='view-btn'
 											value={exercise.id}
@@ -109,60 +111,59 @@ export default function ViewFavorite() {
 											variant='contained'>
 											VIEW
 										</Button>
-									</td>
-									<td>
 										<Button
 											className='delete-btn'
 											value={exercise.id}
 											variant='contained'
-											onClick={DeleteExercise}>
+											onClick={DeleteExercise}
+											style={{
+												backgroundColor: '#De1c06',
+											}}>
 											DELETE
 										</Button>
-									</td>
-								</tr>
-							</table>
+									</Stack>
+								</Grid>
+							</Grid>
 						);
 					} else {
 						return (
-							<table className='Exercise-content'>
-								<tr key={exercise.id} id='row-content'>
-									<td id='name'>{exercise.exercise_name} </td>
-									<td id='target-muscle'> {exercise.muscle_target} </td>
-								</tr>
-								<tr className='button-options'>
-									<td>
+							<Grid className='Exercise-content'>
+								<Grid key={exercise.id} id='row-content'>
+									<MenuItem id='name'>{exercise.exercise_name} </MenuItem>
+									<MenuItem id='target-muscle'>
+										{exercise.muscle_target}
+									</MenuItem>
+								</Grid>
+								<Grid alignItems='center'>
+									<Stack className='button-options' direction='row' gap={4}>
 										<Button
 											className='edit-btn'
 											id='popUp'
 											value={exercise.id}
 											variant='contained'
 											onClick={UpdateLike}>
-											{' '}
-											Like
+											LIKE
 										</Button>
-									</td>
-									<td>
 										<Button
 											className='view-btn'
 											value={exercise.id}
 											onClick={DisplayAnimation}
-											variant='contained'
-											type='button'>
+											variant='contained'>
 											VIEW
 										</Button>
-									</td>
-
-									<td>
 										<Button
 											className='delete-btn'
 											value={exercise.id}
 											variant='contained'
-											onClick={DeleteExercise}>
+											onClick={DeleteExercise}
+											style={{
+												backgroundColor: '#De1c06',
+											}}>
 											DELETE
 										</Button>
-									</td>
-								</tr>
-							</table>
+									</Stack>
+								</Grid>
+							</Grid>
 						);
 					}
 				})}
