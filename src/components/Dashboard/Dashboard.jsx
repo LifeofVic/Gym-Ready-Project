91b73181e6cb03a19c7efd5a8322648fc304bb06
Victor Llapa <victorllapa@gmail.com>
2023-02-20 22:15:47 -0600

    added some styling to the main dashboard by implementing Material's ui components

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
-import '../Dashboard/Dashboard.css';
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { Box, Button } from '@mui/material';
-import SaveIcon from '@mui/icons-material/Save';
-import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
-import { Grid } from '@mui/material';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "../Dashboard/Dashboard.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { Box, Button, Card, Typography } from "@mui/material";
+import SaveIcon from "@mui/icons-material/Save";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import { Grid } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Dashboard() {
 	// this allows us to use <App /> in index.js
@@ -25,17 +25,17 @@ function Dashboard() {
 		const max = 1327;
 		const min = 1;
 		const random = Math.floor(Math.random() * (max - min) + min);
-		console.log('Random Number is:  ', Number(random));
+		console.log("Random Number is:  ", Number(random));
 		setRandomNumber(random);
 	};
 
 	const saveExercise = () => {
-		console.log('Clicked on Save');
-		toast.success('Successfully added to Favorites!', {
+		console.log("Clicked on Save");
+		toast.success("Successfully added to Favorites!", {
 			position: toast.POSITION.BOTTOM_CENTER,
 		});
 		dispatch({
-			type: 'SET_FAVORITE',
+			type: "SET_FAVORITE",
 			payload: {
 				user: user.id,
 				exercise: AllExercises[RandomNumber].id,
@@ -45,7 +45,7 @@ function Dashboard() {
 
 	useEffect(() => {
 		dispatch({
-			type: 'GENERATE_RANDOM_EXERCISE',
+			type: "GENERATE_RANDOM_EXERCISE",
 		});
 	}, []);
 
@@ -80,26 +80,26 @@ function Dashboard() {
 				</Box>
 
 				{/* < className='suggestion-container' onClick={GoToSearch}> */}
-				<div className='suggestion-container'>
-					<div className='home-exercise-gif'>
-						<h4>Muscle Group: </h4>
-						<p>{AllExercises[RandomNumber].muscle_group}</p>
-					</div>
+				<Card className='suggestion-container' raised={true}>
+					<Box className='home-exercise-gif'>
+						<Typography>Muscle Group: </Typography>
+						<Typography>{AllExercises[RandomNumber].muscle_group}</Typography>
+					</Box>
 
-					<div className='home-exercise-group'>
-						<h4> Targeted Muscle: </h4>
-						<p>{AllExercises[RandomNumber].muscle_target}</p>
-					</div>
+					<Box className='home-exercise-group'>
+						<Typography> Targeted Muscle: </Typography>
+						<Typography>{AllExercises[RandomNumber].muscle_target}</Typography>
+					</Box>
 
-					<div className='home-exercise-name'>
-						<h4> Exercise Name: </h4>
-						<p>{AllExercises[RandomNumber].exercise_name}</p>
-					</div>
+					<Box className='home-exercise-name'>
+						<Typography> Exercise Name: </Typography>
+						<Typography>{AllExercises[RandomNumber].exercise_name}</Typography>
+					</Box>
 
-					<div>
+					<Grid>
 						<img src={AllExercises[RandomNumber].gif_url} />
-					</div>
-				</div>
+					</Grid>
+				</Card>
 				<ToastContainer
 					position='bottom-center'
 					autoClose={3000}
