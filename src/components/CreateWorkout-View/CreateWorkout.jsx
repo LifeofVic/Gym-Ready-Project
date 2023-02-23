8be827350b7e8ccdb9a8ae5c699ecca2356c55ad
Victor Llapa <victorllapa@gmail.com>
2023-02-23 00:29:09 -0600

    added some styling to how the drop down menu appears on the DOM

--- src/components/CreateWorkout-View/CreateWorkout.jsx
+++ src/components/CreateWorkout-View/CreateWorkout.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
-import '../CreateWorkout-View/CreateWorkout.css';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import SaveIcon from '@mui/icons-material/Save';
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
-import { AlertTitle, Grid } from '@mui/material';
-import Box from '@mui/material/Box';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import "../CreateWorkout-View/CreateWorkout.css";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+import { AlertTitle, Card, Grid, InputLabel } from "@mui/material";
+import Box from "@mui/material/Box";
+import { ToastContainer, toast } from "react-toastify";
+
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+
+import "react-toastify/dist/ReactToastify.css";
 
 export default function CreateWorkout() {
 	const dispatch = useDispatch();
@@ -32,23 +35,24 @@ export default function CreateWorkout() {
 
 	const history = useHistory();
 
-	const [GroupKeyword, setGroupKeyword] = useState('');
-	const [TargetKeyword, setTargetKeyword] = useState('');
+	const [GroupKeyword, setGroupKeyword] = useState("");
+	const [TargetKeyword, setTargetKeyword] = useState("");
+	const [ExerciseName, setExerciseName] = useState("");
 
 	// This will send the muscle_group value of data type (string) to the exercise.Saga and set result.row to the [store.exercise] where all the exercises with that muscle_group will store in.   GroupedExercise.
 	const ExerciseByGroup = e => {
 		e.preventDefault();
 		setGroupKeyword(e.target.value);
-		console.log('Value is: ', e.target.value);
-		dispatch({ type: 'EXERCISE_BY_GROUP', payload: e.target.value });
+		console.log("Value is: ", e.target.value);
+		dispatch({ type: "EXERCISE_BY_GROUP", payload: e.target.value });
 	};
 
 	const ExerciseByTarget = e => {
-		console.log('In Second Drop Down');
+		console.log("In Second Drop Down");
 		setTargetKeyword(e.target.value);
 
 		dispatch({
-			type: 'FILTER_EXERCISES_BY_KEYWORDS',
+			type: "FILTER_EXERCISES_BY_KEYWORDS",
 			payload: { GroupKeyword: GroupKeyword, TargetKeyword: e.target.value },
 		});
 	};
@@ -56,21 +60,21 @@ export default function CreateWorkout() {
 	//with the id value as the one event.target.value
 	const SetValues = event => {
 		setExerciseId(Number(event.target.value));
-		console.log('Selected Exercise ID is: ', event.target.value);
+		console.log("Selected Exercise ID is: ", event.target.value);
 
 		let findGif = FilterExercises.filter(
 			object => object.id == Number(event.target.value)
 		);
 		setExerciseGif(findGif[0].gif_url);
-		console.log('GIF URL SELECTED IS: ', findGif[0]);
+		console.log("GIF URL SELECTED IS: ", findGif[0]);
 	};
 	//This will run where the event handler will set those values into the corresponding local state to be used in the [AddExercise] once the 'Add Exercise' button is clicked on.
 	//TODO FIX THIS ALSO
 	const AddExercise = () => {
 		//!conditional where the 2 drop down menus is not selected then display alert.
-		if (GroupKeyword != '' && TargetKeyword != '' && ExerciseId != '') {
+		if (GroupKeyword != "" && TargetKeyword != "" && ExerciseId != "") {
 			setAlert(true);
-			toast.success('Successfully added to Favorites!', {
+			toast.success("Successfully added to Favorites!", {
 				position: toast.POSITION.BOTTOM_CENTER,
 			});
 			const exerciseObject = FilterExercises.filter(
@@ -78,23 +82,23 @@ export default function CreateWorkout() {
 			);
 
 			console.log(
-				'exercise object is: ',
+				"exercise object is: ",
 				exerciseObject[0].id,
-				'user data:',
+				"user data:",
 				user.id
 			);
 			dispatch({
-				type: 'SET_FAVORITE',
+				type: "SET_FAVORITE",
 				payload: { exercise: exerciseObject[0].id, user: user.id },
 			});
-			document.getElementById('muscle-group').selectedIndex = 0;
-			document.getElementById('exercise-list-by-group').selectedIndex = 0;
-			document.getElementById('Target-group-selector').selectedIndex = 0;
-			setExerciseGif('');
-			setExerciseId('');
+			document.getElementById("muscle-group").selectedIndex = 0;
+			document.getElementById("exercise-list-by-group").selectedIndex = 0;
+			document.getElementById("Target-group-selector").selectedIndex = 0;
+			setExerciseGif("");
+			setExerciseId("");
 		} else {
-			console.log('UNABLE TO ADD BLANK ITEMS');
-			toast.warn('Please Select an Exercise!', {
+			console.log("UNABLE TO ADD BLANK ITEMS");
+			toast.warn("Please Select an Exercise!", {
 				position: toast.POSITION.BOTTOM_CENTER,
 			});
 		}
@@ -110,86 +114,101 @@ export default function CreateWorkout() {
 			<Grid align='center'>
 				<h1 className='Search-Exercise-Header'>Search Exercise 🔎</h1>
 			</Grid>
-			<div className='filter-section'>
-				<select
-					placeholder='Muscle-Group'
-					id='muscle-group'
-					onChange={ExerciseByGroup}>
-					<option>Select Muscle Group</option>
-					<option value='chest' key={1}>
-						Chest
-					</option>
-					<option value='back' key={2}>
-						Back
-					</option>
-					<option value='cardio' key={3}>
-						Cardio
-					</option>
-					<option value='lower arms' key={4}>
-						Lower Arms
-					</option>
-					<option value='waist' key={6}>
-						Waist
-					</option>
-					<option value='shoulders' key={7}>
-						Shoulders
-					</option>
-					<option value='lower legs' key={8}>
-						Lower Legs
-					</option>
-					<option value='neck' key={9}>
-						Neck
-					</option>
-					<option value='upper arms' key={10}>
-						Upper Arms
-					</option>
-					<option value='upper legs' key={11}>
-						Upper Legs
-					</option>
-				</select>
+			<Card className='filter-section' raised={true}>
+				<Box alignContent={"center"} textAlign='center'>
+					<Box>
+						<InputLabel>Select Muscle Group</InputLabel>
+						<Select
+							label={"Select Muscle group"}
+							value={GroupKeyword}
+							id='muscle-group'
+							onChange={ExerciseByGroup}
+							sx={{ minWidth: "50%" }}>
+							<MenuItem value='chest' key='chest'>
+								Chest
+							</MenuItem>
+							<MenuItem value='back' key='back'>
+								Back
+							</MenuItem>
+							<MenuItem value='cardio' key='cardio'>
+								Cardio
+							</MenuItem>
+							<MenuItem value='lower arms' key='lower arms'>
+								Lower Arms
+							</MenuItem>
+							<MenuItem value='waist' key='waist'>
+								Waist
+							</MenuItem>
+							<MenuItem value='shoulders' key='shoulder'>
+								Shoulders
+							</MenuItem>
+							<MenuItem value='lower legs' key='lower legs'>
+								Lower Legs
+							</MenuItem>
+							<MenuItem value='neck' key='neck'>
+								Neck
+							</MenuItem>
+							<MenuItem value='upper arms' key='upper arms'>
+								Upper Arms
+							</MenuItem>
+							<MenuItem value='upper legs' key='upper legs'>
+								Upper Legs
+							</MenuItem>
+						</Select>
+					</Box>
+					<Box>
+						<InputLabel>Select Specific Muscle</InputLabel>
+						<Select
+							id='Target-group-selector'
+							value={TargetKeyword.muscle_target}
+							onChange={ExerciseByTarget}
+							sx={{ minWidth: "50%" }}>
+							<MenuItem>Select Specific Muscle </MenuItem>
+							{GroupedExercise.map((exercise, index) => {
+								return (
+									<MenuItem
+										className='Drop-List'
+										key={index}
+										value={exercise}
+										id={exercise.muscle_target}>
+										{exercise.muscle_target}
+									</MenuItem>
+								);
+							})}
+						</Select>
+					</Box>
+					<Box>
+						<InputLabel>Select Exercise </InputLabel>
+						<Select
+							className='exercise-list'
+							id='exercise-list-by-group'
+							value={ExerciseName.muscle_target}
+							onChange={SetValues}
+							sx={{ width: "100vw", height: "30px" }}>
+							{FilterExercises.map(exercise => {
+								return (
+									<MenuItem
+										className='Drop-List'
+										key={exercise.id}
+										value={exercise.id}>
+										{exercise.exercise_name}
+									</MenuItem>
+								);
+							})}
+						</Select>
+					</Box>
+				</Box>
+			</Card>
 
-				<select
-					placeholder='Target Muscle'
-					id='Target-group-selector'
-					onChange={ExerciseByTarget}>
-					<option>Select Specific Muscle </option>
-					{GroupedExercise.map((exercise, index) => {
-						return (
-							<option className='Drop-List' key={index} value={exercise.id}>
-								{exercise.muscle_target}
-							</option>
-						);
-					})}
-				</select>
-
-				<select
-					placeholder='Exercise'
-					className='exercise-list'
-					id='exercise-list-by-group'
-					onChange={SetValues}>
-					<option>Select Exercise </option>
-					{FilterExercises.map(exercise => {
-						return (
-							<option
-								className='Drop-List'
-								key={exercise.id}
-								value={exercise.id}>
-								{exercise.exercise_name}
-							</option>
-						);
-					})}
-				</select>
-			</div>
-			{/* {JSON.stringify({ ExerciseGif })} */}
-			<div className='exercise-detail'>
+			<Card className='exercise-detail' raised={true}>
 				<img src={ExerciseGif} />
-			</div>
+			</Card>
 			<Box textAlign='center'>
 				<Button
 					className='submit-btn'
 					onClick={AddExercise}
 					variant='contained'
-					style={{ color: 'white' }}>
+					style={{ color: "white" }}>
 					Add To Favorite
 				</Button>
 			</Box>
--- src/redux/sagas/exercise.saga.js
+++ src/redux/sagas/exercise.saga.js
@@ -1,70 +1,70 @@
-import { put, takeLatest } from 'redux-saga/effects';
-import axios from 'axios';
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
 
 function* exerciseSaga() {
-	yield takeLatest('EXERCISE_BY_GROUP', ExerciseByGroup);
-	yield takeLatest('INSERT_GROUPED_EXERCISE', InsertByGroup);
-	yield takeLatest('FILTER_EXERCISES_BY_KEYWORDS', KeywordFilters);
-	yield takeLatest('GENERATE_RANDOM_EXERCISE', RandomExercise);
+	yield takeLatest("EXERCISE_BY_GROUP", ExerciseByGroup);
+	yield takeLatest("INSERT_GROUPED_EXERCISE", InsertByGroup);
+	yield takeLatest("FILTER_EXERCISES_BY_KEYWORDS", KeywordFilters);
+	yield takeLatest("GENERATE_RANDOM_EXERCISE", RandomExercise);
 }
 
 //? This will receive the desired exercises by muscle group.
 // Sending the action.payload that contains the string of the muscle_group selected by the user. assigning that to exerciseByGroup
 // exerciseByGroup will then be sent to the reducer by ( yield put ) where it will be set to be used globally.
 function* ExerciseByGroup(action) {
-	console.log('In Exercise by group', '1:', action.type, '2:', action.payload);
+	console.log("In Exercise by group", "1:", action.type, "2:", action.payload);
 	try {
 		const exerciseByGroup = yield axios.get(`/exercise/${action.payload}`);
-		console.log('Exercise By Group is: ', exerciseByGroup.data);
+		console.log("Exercise By Group is: ", exerciseByGroup.data);
 		yield put({
-			type: 'SET_GROUPED_EXERCISE',
+			type: "SET_GROUPED_EXERCISE",
 			payload: exerciseByGroup.data,
 		});
 	} catch (error) {
-		console.log('Error in exercise.saga / ExerciseByGroup / ');
+		console.log("Error in exercise.saga / ExerciseByGroup / ");
 	}
 }
 
 function* InsertByGroup(action) {
-	console.log('In Exercise by group', '1:', action.type, '2:', action.payload);
+	console.log("In Exercise by group", "1:", action.type, "2:", action.payload);
 	try {
 		const exerciseByGroup = yield axios.get(
 			`/exercise/Insert/${action.payload}`
 		);
-		console.log('Exercise By Group is: ', exerciseByGroup.data);
+		console.log("Exercise By Group is: ", exerciseByGroup.data);
 		yield put({
-			type: 'INSERT_EXERCISE',
+			type: "INSERT_EXERCISE",
 			payload: exerciseByGroup.data,
 		});
 	} catch (error) {
-		console.log('Error in exercise.saga / ExerciseByGroup / ');
+		console.log("Error in exercise.saga / ExerciseByGroup / ");
 	}
 }
 
 function* KeywordFilters(action) {
 	console.log(
-		'Keywords: ',
+		"Keywords: ",
 		action.payload.GroupKeyword,
-		action.payload.TargetKeyword
+		action.payload.TargetKeyword.muscle_target
 	);
 	try {
 		const FilteredExercises = yield axios.get(
-			`/exercise/${action.payload.GroupKeyword}/${action.payload.TargetKeyword}`
+			`/exercise/${action.payload.GroupKeyword}/${action.payload.TargetKeyword.muscle_target}`
 		);
-		yield put({ type: 'FILTERED_EXERCISE', payload: FilteredExercises.data });
+		yield put({ type: "FILTERED_EXERCISE", payload: FilteredExercises.data });
 	} catch (error) {
-		console.log('Error in KeywordFilter: ', error);
+		console.log("Error in KeywordFilter: ", error);
 	}
 }
 
 function* RandomExercise(action) {
-	console.log('Random exercise generator: ');
+	console.log("Random exercise generator: ");
 
 	try {
 		const ExerciseArray = yield axios.get(`/exercise`);
-		yield put({ type: 'ALL_EXERCISES_ARRAY', payload: ExerciseArray.data });
+		yield put({ type: "ALL_EXERCISES_ARRAY", payload: ExerciseArray.data });
 	} catch (error) {
-		console.log('Error in Random Exercise generator');
+		console.log("Error in Random Exercise generator");
 	}
 }
 export default exerciseSaga;
